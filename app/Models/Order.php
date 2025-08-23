586b266a233442898cd8cef2f1622bfcfca53c17
OrryFrasetyo <orrytio12345@gmail.com>
2025-08-23 17:58:22 +0700

    Add models, migrations, and seeders for orders and order items; update user factory and category model

---
+++ app/Models/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['order_code','user_id', 'subtotal', 'tax', 'grand_total', 'status', 'table_number', 'payment_method', 'note', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
